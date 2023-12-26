f6126c0b1c7dbe5265848763a51a73e7e999e21e
velramiir <justus@zeinert.dev>
2023-12-26 12:46:54 +0100

    chore: add direnv and flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A Nix-flake-based Node development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ nodejs_21 figlet ];
+
+          shellHook = "
+            figlet 'Push the Button'
+          ";
+        };
+      });
+}
